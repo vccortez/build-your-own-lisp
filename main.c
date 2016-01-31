297c1832d0ab9d1755c8718d0631798e133a7048
Vekat <vitoracortez+c9@gmail.com>
2016-01-31 21:22:06 +0000

    Complete chapter 9

--- main.c
+++ main.c
@@ -28,9 +28,12 @@ lval* lval_read_num(mpc_ast_t*);
 lval* lval_read(mpc_ast_t*);
 lval* lval_add(lval*, lval*);
 
-lval eval(mpc_ast_t*);
-lval eval_op(lval, char*, lval);
-lval eval_unary_op(lval, char*);
+lval* lval_eval_sexpr(lval*);
+lval* lval_eval(lval*);
+lval* lval_pop(lval*, int);
+lval* lval_take(lval*, int);
+
+lval* builtin_op(lval*, char*);
 
 lval* lval_num(double);
 lval* lval_err(char*);
@@ -58,7 +61,7 @@ int main() {
       symbol : '+' | '-' | '*' | '/' | '%' | '^' | \"min\" | \"max\" ;\
       sexpr  : '(' <expr>* ')' ;\
       expr   : <number> | <symbol> | <sexpr> ;\
-      lispy  : /^/ <symbol> <expr>+ /$/ ;\
+      lispy  : /^/ <expr>* /$/ ;\
       ",
       Number, Symbol, Sexpr, Expr, Lispy);
   // version and exit information
@@ -79,10 +82,9 @@ int main() {
     // attempt to parse user input
     mpc_result_t r;
     if (mpc_parse("<stdin> ", input, Lispy, &r)) {
-      // lval answer = eval(r.output);
-      lval* x = lval_read(r.output);
-      lval_println(x);
-      lval_del(x);
+      lval* answer = lval_eval(lval_read(r.output));
+      lval_println(answer);
+      lval_del(answer);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
@@ -97,93 +99,125 @@ int main() {
   return on_exit();
 }
 
-/*
-lval eval(mpc_ast_t* t) {
-  // if number return it
-  if (strstr(t->tag, "number")) {
-    // check error in conversion
-    errno = 0;
-    //long x = strtol(t->contents, NULL, 10);
-    double x = strtod(t->contents, NULL);
-    return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+lval* lval_eval_sexpr(lval* v) {
+  // evaluate children
+  for (int i = 0; i < v-> count; ++i) {
+    v->cell[i] = lval_eval(v->cell[i]);
   }
 
-  // symbol is always the second child
-  char* op = t->children[1]->contents;
-
-  // store the third child in 'x'
-  lval x = eval(t->children[2]);
-
-  // iterate remaining children and combining
-  // or if no remaining children attempt unary operation
-  int i = 3;
-  if (strstr(t->children[i]->tag, "expr")) {
-    while (strstr(t->children[i]->tag, "expr")) {
-      x = eval_op(x, op, eval(t->children[i]));
-      i++;
+  // error checking
+  for (int i = 0; i < v->count; ++i) {
+    if (v->cell[i]->type == LVAL_ERR) {
+      return lval_take(v, i);
     }
-  } else {
-    x = eval_unary_op(x, op);
   }
 
+  // empty expression
+  if (v->count == 0) {
+    return v;
+  }
+
+  // single expression
+  if (v->count == 1) {
+    return lval_take(v, 0);
+  }
+
+  // ensure first element is symbol
+  lval* f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f);
+    lval_del(v);
+    return lval_err("s-expression missing symbol!");
+  }
+
+  // call builtin with operator
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
+}
+
+lval* lval_eval(lval* v) {
+  // evaluate if s-expressions
+  if (v->type == LVAL_SEXPR) {
+    return lval_eval_sexpr(v);
+  }
+  // else return itself
+  return v;
+}
+
+lval* lval_pop(lval* v, int i) {
+  // find item at 'i'
+  lval* x = v->cell[i];
+
+  // shift memory after the item 'i' over the top
+  memmove(&v->cell[i], &v->cell[i+1], sizeof(lval*) * (v->count-i-1));
+
+  // decrease the item count
+  v->count--;
+
+  // reallocate the memory
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
   return x;
 }
 
-lval eval_op(lval x, char* op, lval y) {
-  // if either is an error, return it
-  if (x.type == LVAL_ERR) { return x; }
-  if (y.type == LVAL_ERR) { return y; }
-
-  // else do the math
-  int err = -1;
-  double num;
-  if (strcmp(op, "+") == 0) {
-    num = x.num + y.num;
-  }
-  else if (strcmp(op, "-") == 0) {
-    num = x.num - y.num;
-  }
-  else if (strcmp(op, "*") == 0) {
-    num = x.num * y.num;
-  }
-  else if (strcmp(op, "/") == 0) {
-    if (y.num == 0) {
-      err = LERR_DIV_ZERO;
-    } else {
-      num = x.num / y.num;
-    }
-  }
-  else if (strcmp(op, "%") == 0) {
-    if (y.num == 0) {
-      err = LERR_DIV_ZERO;
-    } else {
-      num = fmod(x.num, y.num);
-    }
-  }
-  else if (strcmp(op, "^") == 0) {
-    num = pow(x.num, y.num);
-  }
-  else if (strcmp(op, "min") == 0) {
-    num = (x.num > y.num) ? y.num : x.num;
-  }
-  else if (strcmp(op, "max") == 0) {
-    num = (x.num > y.num) ? x.num : y.num;
-  }
-
-  if (err != -1) {
-    return lval_err(err);
-  } else {
-    return lval_num(num);
-  }
-}
-
-lval eval_unary_op(lval x, char* op) {
-  if (strcmp(op, "-") == 0) {
-    return lval_num(x.num * -1);
-  }
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
   return x;
 }
-*/
+
+lval* builtin_op(lval* a, char* op) {
+  // ensure all are numbers
+  for (int i = 0; i < a->count; ++i) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("can't operate on non-number");
+    }
+  }
+
+  // pop first element
+  lval* x = lval_pop(a, 0);
+
+  // if no more args and subtracting then negate
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num *= -1;
+  }
+
+  // while there are other elements
+  while(a->count > 0) {
+    // pop next element
+    lval* y = lval_pop(a, 0);
+
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x);
+        lval_del(y);
+        x = lval_err("division by zero!");
+        break;
+      }
+      x->num /= y->num;
+    }
+    if (strcmp(op, "%") == 0) {
+      if (y->num == 0) {
+        lval_del(x);
+        lval_del(y);
+        x = lval_err("division by zero!");
+        break;
+      }
+      x->num = fmod(x->num, y->num);
+    }
+    if (strcmp(op, "^") == 0) { x->num = pow(x->num, y->num); }
+    if (strcmp(op, "min") == 0) { x->num = fmin(x->num, y->num); }
+    if (strcmp(op, "max") == 0) { x->num = fmax(x->num, y->num); }
+    lval_del(y);
+  }
+
+  lval_del(a);
+  return x;
+}
 
 lval* lval_num(double x) {
   lval* v = malloc(sizeof(lval));
